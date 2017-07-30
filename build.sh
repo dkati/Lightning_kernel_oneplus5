8df25b1635fd8e3090a115bf2fbc6ca0c5e60ca3
nikhil18 <nikhilgolechha1@gmail.com>
2017-07-30 02:09:02 -0700

    bump to version 6

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ KERNEL="Image.gz-dtb"
 DEFCONFIG="lightning_defconfig"
 
 # Kernel Details
-VER=Lightning-Kernel-V5
-VARIANT="OP5-OOS-N-V5"
+VER=Lightning-Kernel-V6
+VARIANT="OP5-OOS-N-V6"
 
 # Vars
 export LOCALVERSION=~`echo $VER`
