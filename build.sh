11c7190a4a2e339f88710f6ea5cae0484049f814
dkati <dkatikaridis@gmail.com>
2017-08-19 21:58:14 +0300

    build.sh:Our build script
    Change-Id: I735af1df5bdce7ffd5645d8c4ab2d7fcc43e6ae3

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ KERNEL="Image.gz-dtb"
 DEFCONFIG="lightning_defconfig"
 
 # Kernel Details
-VER=Lightning-Kernel-V9
-VARIANT="OP5-LOS-N-V9"
+VER=Lightning-Kernel-V8
+VARIANT="OP5-LOS-N-V8"
 
 # Vars
 export LOCALVERSION=~`echo $VER`
@@ -21,11 +21,11 @@ export ARCH=arm64
 export SUBARCH=arm64
 
 # Paths
-KERNEL_DIR="${HOME}/android/Lightning_kernel_oneplus5"
-REPACK_DIR="${HOME}/android/anykernel2los/"
-PATCH_DIR="${HOME}/android/anykernel2los/patch/"
-MODULES_DIR="${HOME}/android/anykernel2los/modules/"
-ZIP_MOVE="${HOME}/android/zip"
+KERNEL_DIR="${HOME}/android/op5cm14/kernel/oneplus/lightning"
+REPACK_DIR="${HOME}/android/op5cm14/kernel/oneplus/anykernel"
+PATCH_DIR="${HOME}/android/op5cm14/kernel/oneplus/anykernel/patch/"
+MODULES_DIR="${HOME}/android/op5cm14/kernel/oneplus/anykernel/modules/"
+ZIP_MOVE="${HOME}/android/op5cm14"
 ZIMAGE_DIR="$KERNEL_DIR/arch/arm64/boot"
 
 function clean_all {
@@ -61,10 +61,10 @@ function make_zip {
 DATE_START=$(date +"%s")
 
 echo "Build Script:"
-export CROSS_COMPILE=${HOME}/android/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=${HOME}/android/op5cm14/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 echo
 
-while read -p "Do you want to clean stuffs (y/n)? " cchoice
+while read -p "Do you want to clean (y/n)? " cchoice
 do
 case "$cchoice" in
 	y|Y )
